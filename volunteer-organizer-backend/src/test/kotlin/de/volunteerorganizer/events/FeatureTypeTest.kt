0b4e36a8a8e8b2ada37660a652bf110e23cd92b1
benicamera <66155472+benicamera@users.noreply.github.com>
2024-02-11 19:57:37 +0100

    Fix relevant part of KT-W1042

--- volunteer-organizer-backend/src/test/kotlin/de/volunteerorganizer/events/FeatureTypeTest.kt
+++ volunteer-organizer-backend/src/test/kotlin/de/volunteerorganizer/events/FeatureTypeTest.kt
@@ -3,29 +3,32 @@ package de.volunteerorganizer.events
 import junit.framework.TestCase
 
 class FeatureTypeTest : TestCase() {
+    private val testName1 = "VeryCoolName"
+    private val testName2 = "VeryUncoolName"
+
     fun testTestGetName() {
-        val type = FeatureType("VeryCoolName", 10)
-        assertEquals(type.name, "VeryCoolName")
+        val type = FeatureType(testName1, 10)
+        assertEquals(type.name, testName1)
     }
 
     fun testGetId() {
-        val type = FeatureType("VeryCoolName", 10)
+        val type = FeatureType(testName1, 10)
         assertEquals(type.id, 10)
     }
 
     fun testCopy() {
-        val type1 = FeatureType("VeryCoolName", 10)
+        val type1 = FeatureType(testName1, 10)
         val typeCopy = type1.copy()
         assertEquals(type1.id, typeCopy.id)
         assertEquals(type1.name, typeCopy.name)
     }
 
     fun testTestEquals() {
-        val type1 = FeatureType("VeryCoolName", 10)
-        val type2 = FeatureType("VeryCoolName", 10)
+        val type1 = FeatureType(testName1, 10)
+        val type2 = FeatureType(testName1, 10)
 
-        val type3 = FeatureType("VeryCoolName", 11)
-        val type4 = FeatureType("VeryUncoolName", 10)
+        val type3 = FeatureType(testName1, 11)
+        val type4 = FeatureType(testName2, 10)
 
         assertTrue(type1 == type2)
         assertFalse(type1 == type3)
