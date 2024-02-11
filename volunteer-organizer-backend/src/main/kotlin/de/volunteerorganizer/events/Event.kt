c54ff7a49d9efa81c6d2d0a35774a86ac07944a2
benicamera <66155472+benicamera@users.noreply.github.com>
2024-02-11 13:16:18 +0100

    Add the Event class

---
+++ volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/events/Event.kt
@@ -0,0 +1,29 @@
+package de.volunteerorganizer.events
+
+import java.util.*
+
+class Event: IEvent {
+    override fun changeName(newName: EventName) {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeDate(newDate: Date) {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeLocation(newLocation: EventLocation) {
+        TODO("Not yet implemented")
+    }
+
+    override fun changeRule(newRule: EventRule) {
+        TODO("Not yet implemented")
+    }
+
+    override fun addVolunteer(newVolunteer: EventVolunteer) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeVolunteer(volunteer: EventVolunteer) {
+        TODO("Not yet implemented")
+    }
+}
