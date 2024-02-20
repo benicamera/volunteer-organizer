19478ee9892c5fdcbc65b777f0a0f245372cd41d
benicamera <66155472+benicamera@users.noreply.github.com>
2024-02-20 14:03:14 +0100

    Add requirement to ValidIntRange

--- volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/events/ValidIntRange.kt
+++ volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/events/ValidIntRange.kt
@@ -6,5 +6,11 @@ package de.volunteerorganizer.events
  * @param maxValue: upper bound of interval (inclusive)
  */
 class ValidIntRange(private val minValue: Int, private val maxValue: Int) : IValidValues<Int> {
+    init {
+        require(minValue <= maxValue) {
+            "$minValue is bigger than $maxValue => ${::minValue.name} is bigger than ${::maxValue.name}"
+        }
+    }
+
     override fun isValid(value: Int): Boolean = value in minValue..maxValue
 }
--- volunteer-organizer-backend/src/test/kotlin/de/volunteerorganizer/events/ValidIntRangeTest.kt
+++ volunteer-organizer-backend/src/test/kotlin/de/volunteerorganizer/events/ValidIntRangeTest.kt
@@ -3,19 +3,33 @@ package de.volunteerorganizer.events
 import junit.framework.TestCase
 
 class ValidIntRangeTest : TestCase() {
-    fun testIsValid() {
-        val upper = 10
-        val lower = 0
-        val testRange = ValidIntRange(lower, upper)
+    private var upperBound = 10
+    private var lowerBound = 0
+    private var testRange = ValidIntRange(lowerBound, upperBound)
 
-        val shouldBeTrue1 = testRange.isValid(lower)
-        val shouldBeTrue2 = testRange.isValid(upper)
-        val shouldBeTrue3 = testRange.isValid((lower + upper) / 2)
-        val shouldBeFalse = testRange.isValid(upper * 2)
+    fun testIsValidSimple() {
+        val shouldBeTrue3 = testRange.isValid((upperBound + lowerBound) / 2)
+        assertTrue(shouldBeTrue3)
+    }
 
+    fun testIsValidEdge() {
+        val shouldBeTrue1 = testRange.isValid(upperBound)
+        val shouldBeTrue2 = testRange.isValid(lowerBound)
         assertTrue(shouldBeTrue1)
         assertTrue(shouldBeTrue2)
-        assertTrue(shouldBeTrue3)
+    }
+
+    fun testIsValidFalse() {
+        val shouldBeFalse = testRange.isValid(upperBound * 2)
         assertFalse(shouldBeFalse)
+    }
+
+    fun testInitError() {
+        try {
+            ValidIntRange(upperBound, lowerBound)
+        } catch (err: IllegalArgumentException) {
+            return
+        }
+        fail()
     }
 }
