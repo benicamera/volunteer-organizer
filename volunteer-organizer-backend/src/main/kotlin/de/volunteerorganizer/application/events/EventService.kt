570772c2aa2769e71125c0e4461e237c7101eae1
benicamera <66155472+benicamera@users.noreply.github.com>
2024-03-21 15:29:52 +0100

    Restructure file

--- volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/application/events/EventService.kt
+++ volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/application/events/EventService.kt
@@ -1,22 +1,22 @@
 package de.volunteerorganizer.application.events
 
 import de.volunteerorganizer.domain.event.*
-import de.volunteerorganizer.domain.event.location.EventLocation
 
 /**
  * Service covering event creation, event edit and event listing use cases.
+ * TODO: refine to not make it dependant on domain?
  * @param eventRepository repository for events to be used
  */
 class EventService(private val eventRepository: IEventRepository) {
-
     /**
      * Method for creating event use case
      * @param issuerId ID of volunteer issuing event creation
-     * @param eventName name of event to be created
-     * @param timeFrame time frame when the event takes place
-     * @param location location where the event takes place
+     * @param eventInformation Information for event creation. No member of instance can be null
      */
-    fun createEvent(issuerId: Int, eventName: EventName, timeFrame: EventTimeFrame, location: EventLocation) {
+    fun createEvent(
+        issuerId: Int,
+        eventInformation: EventEdit,
+    ) {
         // check if volunteer with issuer id is allowed to create event
 
         // create event instance
@@ -29,7 +29,10 @@ class EventService(private val eventRepository: IEventRepository) {
      * @param issuerId ID of volunteer issuing event deletion
      * @param eventId ID of event to be deleted
      */
-    fun deleteEvent(issuerId: Int, eventId: Int) {
+    fun deleteEvent(
+        issuerId: Int,
+        eventId: Int,
+    ) {
         // check if volunteer with issuer id is allowed to delete event
 
         // delete event from repository
@@ -43,7 +46,13 @@ class EventService(private val eventRepository: IEventRepository) {
      * @param timeFrame time frame of the task
      * @param featureRequirements requirements for the task
      */
-    fun addNewTaskToEvent(issuerId: Int, eventId: Int, taskName: String, timeFrame: EventTimeFrame, featureRequirements: Set<FeatureRequirement>) {
+    fun addNewTaskToEvent(
+        issuerId: Int,
+        eventId: Int,
+        taskName: String,
+        timeFrame: EventTimeFrame,
+        featureRequirements: Set<FeatureRequirement>,
+    ) {
         // check if volunteer with issuer id is allowed to add task to event
 
         // create task
@@ -59,7 +68,11 @@ class EventService(private val eventRepository: IEventRepository) {
      * @param eventId ID of event from which task is removed
      * @param taskId ID of task to be removed
      */
-    fun removeTaskFromEvent(issuerId: Int, eventId: Int, taskId: Int) {
+    fun removeTaskFromEvent(
+        issuerId: Int,
+        eventId: Int,
+        taskId: Int,
+    ) {
         // check if volunteer with issuer id is allowed to remove task from event
 
         // get event instance
@@ -76,7 +89,11 @@ class EventService(private val eventRepository: IEventRepository) {
      * @param eventId ID of event to be edited
      * @param eventEdit edits to be made
      */
-    fun editEvent(issuerId: Int, eventId: Int, eventEdit: EventEdit) {
+    fun editEvent(
+        issuerId: Int,
+        eventId: Int,
+        eventEdit: EventEdit,
+    ) {
         // check if volunteer with issuerId is allowed to edit the event
 
         // get event instance
@@ -89,7 +106,7 @@ class EventService(private val eventRepository: IEventRepository) {
     /**
      * Method for event listing use case
      * @param issuerId ID of volunteer issuing the use case
-     * @returns set of event for user
+     * @returns set of event for user TODO: maybe refine output to make it not dependant on domain?
      */
     fun getAllEvents(issuerId: Int): Set<Event> {
         // get all events from repository for issuerId
