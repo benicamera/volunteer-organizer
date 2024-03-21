930ae67b98626ecf66103f3d0cb0082d344a218f
benicamera <contact.benix@gmail.com>
2024-03-21 11:33:57 +0100

    Add Statistics data class

---
+++ volunteer-organizer-backend/src/main/kotlin/de/volunteerorganizer/application/Statistics.kt
@@ -0,0 +1,7 @@
+package de.volunteerorganizer.application
+
+/**
+ * @param registrations number of registrations
+ * @param noRegistrations possibilities of registration - number of actual registrations
+ */
+data class Statistics(val registrations: Int, val noRegistrations: Int)
